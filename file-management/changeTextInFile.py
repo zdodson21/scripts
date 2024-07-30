694c128a40c7ac023a97f08bfdae6f68d186ad3d
Zach Dodson <zdodson21@gmail.com>
2024-07-30 09:06:14 -0400

    changeTextInFile complete

--- file-management/changeTextInFile.py
+++ file-management/changeTextInFile.py
@@ -1,9 +1,34 @@
+# Imports
+import pathlib
+from os import path
+
 # This file serves the purpose of navigating a file and changing specified text within the file
-print('Running changeTextInFile.py...')
+print('Running changeTextInFile.py (Use CTRL + C to exit)')
+
 
 # Variables:
 
-filePath = input('File Path: ') # set file path here
-currentText = input('Current Text (the text you want replaced): ') # set text you want to change here
-replacementText = input('Replacement Text: ') # set replacement text here
+startingPath = pathlib.Path.home()
+filePath = input(f'File Name (starting at {startingPath}): ') # set file path here
+finalFilePath = path.join(startingPath, filePath)
+print (f'Selected filePath: {finalFilePath}')
 
+currentText = input('Current Text (the text you want replaced): ') # set text you want to change here
+print(f'Replacable text set to: {currentText}')
+
+replacementText = input('Replacement Text: ') # set replacement text here
+print(f'Replacement text set to: {replacementText}')
+
+
+# Code:
+print('Running...')
+
+with open(finalFilePath, 'r') as file:
+  filedata = file.read()
+
+filedata = filedata.replace(currentText, replacementText)
+
+with open(finalFilePath, 'w') as file:
+  file.write(filedata)
+
+print('Done!')
