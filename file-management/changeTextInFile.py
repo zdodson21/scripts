353f9a00b4439dd147550ffd1be4e78317fc71ed
Zach Dodson <zdodson21@gmail.com>
2024-07-30 08:24:57 -0400

    script prepped

---
+++ file-management/changeTextInFile.py
@@ -0,0 +1,9 @@
+# This file serves the purpose of navigating a file and changing specified text within the file
+print('Running changeTextInFile.py...')
+
+# Variables:
+
+filePath = input('File Path: ') # set file path here
+currentText = input('Current Text (the text you want replaced): ') # set text you want to change here
+replacementText = input('Replacement Text: ') # set replacement text here
+
