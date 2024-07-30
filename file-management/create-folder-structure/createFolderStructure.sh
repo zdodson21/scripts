bb9cf1d4ffee5c656a0569dc719eb4124791cf1b
Zach Dodson <zdodson21@gmail.com>
2024-07-30 11:22:24 -0400

    complete, moving for testing

---
+++ file-management/create-folder-structure/createFolderStructure.sh
@@ -0,0 +1,35 @@
+# Directory Variables
+formatDirectory = ~/Documents
+  stylingDirectory = $formatDirectory/styling;;
+  projectsDirectory = $formatDirectory/projects;;
+    artDirectory = $projectsDirectory/art;;
+    developmentDirectory = $projectsDirectory/development;;
+    modellingDirectory = $projectsDirectory/modelling;;
+    musicDirectory = $projectsDirectory/music;;
+    videoDirectory = $projectsDirectory/video;;
+    writingDirectory = $projectsDirectory/writing;;
+
+# Commands
+mkdir -p $stylingDirectory;;
+mkdir -p $projectsDirectory;;
+  mkdir -p $artDirectory;;
+  mkdir -p $developmentDirectory;;
+    mkdir -p $developmentDirectory/applications;;
+    mkdir -p $developmentDirectory/game-dev;;
+    mkdir -p $developmentDirectory/learning-projects;;
+      mkdir -p $developmentDirectory/learning-projects/c;;
+      mkdir -p $developmentDirectory/learning-projects/c#;;
+      mkdir -p $developmentDirectory/learning-projects/c++;;
+      mkdir -p $developmentDirectory/learning-projects/godot;;
+      mkdir -p $developmentDirectory/learning-projects/java;;
+      mkdir -p $developmentDirectory/learning-projects/javascript;;
+      mkdir -p $developmentDirectory/learning-projects/kotlin;;
+      mkdir -p $developmentDirectory/learning-projects/node;;
+      mkdir -p $developmentDirectory/learning-projects/python;;
+      mkdir -p $developmentDirectory/learning-projects/rust;;
+    mkdir -p $developmentDirectory/web-dev;;
+      mkdir -p $developmentDirectory/web-dev//essential-component-toolbox;;
+  mkdir -p $modellingDirectory;;
+  mkdir -p $musicDirectory;;
+  mkdir -p $videoDirectory;;
+  mkdir -p $writingDirectory;;
