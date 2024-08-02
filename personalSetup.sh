e7dd0195bae4c0b84f0b8ed9fd7ab58ded939deb
Zach Dodson <zdodson21@gmail.com>
2024-08-02 08:24:40 -0400

    personal setup

---
+++ personalSetup.sh
@@ -0,0 +1,33 @@
+# Need to test, but should hopefully be able to just run and work.
+
+# Git Preparation
+read -p "Enter your preferred username for Git: " name;
+read -p "Enter your preferred email for Git: " email;
+
+# Prep before install of apps
+sudo apt update -y && sudo apt upgrade -y;
+
+# Install my preferred applications
+sudo apt-get install p7zip-full -y; # 7zip
+sudo apt install curl -y; # curl
+sudo apt install flatpak -y; # flatpak
+sudo apt install gimp -y; # gimp
+sudo apt install git-all -y; # git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; # Node Version Manager (nvm)
+nvm install --lts; # Node LTS & NPM
+npm install -global yarn; # Yarn
+sudo apt install obs-studio -y; # obs-studio
+sudo apt-get install python3.6 -y; # python 3.6
+sudo apt-get install python3-pip -y; # pip
+sudo apt-get install ruby-full -y; # ruby
+sudo apt install vlc -y; # vlc media player
+sudo apt install vim -y; # vim
+sudo apt install wine -y; # wine
+sudo apt install zsh -y; # zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # oh-my-zsh
+
+# Set up Git
+git config --global user.name $name;
+git config --global user.name;
+git config --global user.email $email;
+git config --global user.email;
